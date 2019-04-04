71f8d25acad8b8ceb57541c5a3bb678f88560a56
David C. Rankin <drankinatty@gmail.com>
2019-04-04 01:43:23 -0500

    Update compile instructions in winsolver.cpp to include compiler strings with and without separate ./bin and ./obj directories.

--- winsolver.cpp
+++ winsolver.cpp
@@ -3,6 +3,10 @@ Compile Resource File, then Build Application
 
   rc /Fowinsolver.res winsolver.rc
 
+  cl /nologo /W3 /wd4996 /Ox /Fewinsolver /Tp winsolver.cpp /Tc memrealloc.c /Tc mtrx_t.c user32.lib comctl32.lib gdi32.lib winsolver.res
+
+Or if you create separate ./bin and ./obj subdirectories
+
   cl /nologo /W3 /wd4996 /Ox /Foobj/ /Febin/winsolver /Tp winsolver.cpp /Tc memrealloc.c /Tc mtrx_t.c user32.lib comctl32.lib gdi32.lib winsolver.res
 
 */
@@ -28,8 +32,8 @@ WCHAR szWindowClass[MAX_LOADSTRING];    /* the main window class name */
 HWND hWndEdit;                          /* edit box instance */
 HWND hWndTool;                          /* toolbar instance */
 /* monospace font for edit box */
-HFONT hFont = CreateFont(13, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, 
-                DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, 
+HFONT hFont = CreateFont(13, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE,
+                DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS,
                 CLEARTYPE_QUALITY, FIXED_PITCH, TEXT("DejaVu Sans Mono"));
 HIMAGELIST g_hImageList = NULL;         /* imagelist for toolbar */
 
@@ -153,7 +157,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         hWndEdit = CreateWindow (
                     L"edit",           // The class name required is edit
                     L"Linear System Solver", // Default text, styles below
-                    WS_VISIBLE | WS_CHILD | WS_HSCROLL | WS_VSCROLL| 
+                    WS_VISIBLE | WS_CHILD | WS_HSCROLL | WS_VSCROLL|
                     ES_LEFT | ES_MULTILINE | ES_AUTOHSCROLL,
                     0,0,                    // the left and top co-ordinates
                     0,0,                    // size with WM_SIZE message
@@ -161,7 +165,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     (HMENU)IDM_EDIT,        // the ID of your editbox
                     hInst,                  // the instance of your application
                     NULL                    // extra bits you dont really need
-        
+
         );
         /* set the font to Fixed (DejaVu Sans Mono) */
         SendMessage (hWndEdit, WM_SETFONT, (WPARAM)hFont, MAKELPARAM(true,0));
@@ -260,16 +264,16 @@ HWND CreateSimpleToolbar(HWND hWndParent)
     const int ImageListID    = 0;   /* toolbar constants */
     const int numButtons     = 4;
     const int bitmapSize     = 16;
-    
+
     const DWORD buttonStyles = BTNS_AUTOSIZE;
 
     /* create the toolbar with flat/transparent buttons w/text on side */
-    HWND hWndToolbar = CreateWindowEx(0, TOOLBARCLASSNAME, NULL, 
-                                      WS_CHILD | TBSTYLE_WRAPABLE | 
+    HWND hWndToolbar = CreateWindowEx(0, TOOLBARCLASSNAME, NULL,
+                                      WS_CHILD | TBSTYLE_WRAPABLE |
                                       TBSTYLE_LIST | TBSTYLE_FLAT,
-                                      0, 0, 0, 0, 
+                                      0, 0, 0, 0,
                                       hWndParent, NULL, hInst, NULL);
-        
+
     if (hWndToolbar == NULL)    /* validate toolbar creation */
         return NULL;
 
@@ -279,17 +283,17 @@ HWND CreateSimpleToolbar(HWND hWndParent)
                                     numButtons, 0);
 
     /* set the image list */
-    SendMessage(hWndToolbar, TB_SETIMAGELIST, 
-                (WPARAM)ImageListID, 
+    SendMessage(hWndToolbar, TB_SETIMAGELIST,
+                (WPARAM)ImageListID,
                 (LPARAM)g_hImageList);
 
     /* load the button images */
-    SendMessage(hWndToolbar, TB_LOADIMAGES, 
-                (WPARAM)IDB_STD_SMALL_COLOR, 
+    SendMessage(hWndToolbar, TB_LOADIMAGES,
+                (WPARAM)IDB_STD_SMALL_COLOR,
                 (LPARAM)HINST_COMMCTRL);
 
     /* Initialize button info (can move to resource file) */
-    TBBUTTON tbButtons[numButtons] = 
+    TBBUTTON tbButtons[numButtons] =
     {
         { MAKELONG(STD_DELETE,  ImageListID), IDM_CLEAR,  TBSTATE_ENABLED, buttonStyles, {0}, 0, (INT_PTR)L"Clear" },
         { MAKELONG(STD_PROPERTIES,  ImageListID), IDM_SOLV,  TBSTATE_ENABLED, buttonStyles, {0}, 0, (INT_PTR)L"Solve" },
@@ -302,9 +306,9 @@ HWND CreateSimpleToolbar(HWND hWndParent)
     SendMessage(hWndToolbar, TB_ADDBUTTONS,       (WPARAM)numButtons,       (LPARAM)&tbButtons);
 
     /* resize the toolbar, and display */
-    SendMessage(hWndToolbar, TB_AUTOSIZE, 0, 0); 
+    SendMessage(hWndToolbar, TB_AUTOSIZE, 0, 0);
     ShowWindow(hWndToolbar,  TRUE);
-    
+
     return hWndToolbar;
 }
 
@@ -312,14 +316,14 @@ HWND CreateSimpleToolbar(HWND hWndParent)
 void *szrealloc (void *ptr, const size_t ptrsz, const size_t oldsz, const size_t newsz)
 {
     void *memptr = new char[newsz * ptrsz];
-    
+
     if (newsz > oldsz) {
         memcpy (memptr, ptr, oldsz * ptrsz);
         memset ((char*)memptr + (oldsz * ptrsz), 0, (newsz - oldsz) * ptrsz);
     }
     else
         memcpy (memptr, ptr, newsz * ptrsz);
-    
+
     delete [] ptr;
     return memptr;
 }
@@ -328,17 +332,17 @@ void *szrealloc (void *ptr, const size_t ptrsz, const size_t oldsz, const size_t
 void *szrealloc2 (void *ptr, const size_t ptrsz, size_t *nelem)
 {
     void *memptr = new char[2 * *nelem * ptrsz];
-    
+
     memcpy (memptr, ptr, *nelem * ptrsz);
     memset ((char*)memptr + (*nelem * ptrsz), 0, *nelem * ptrsz);
     *nelem *= 2;
-    
+
     delete [] ptr;
     return memptr;
 }
 
 /* retrieve coefficient matrix from editbox, convert to C-string, and
- * pass to equation solver. retrieve the solution vector, append to 
+ * pass to equation solver. retrieve the solution vector, append to
  * editbox content with header, convert to wchar_t and write back to
  * editbox.
  */
@@ -353,18 +357,18 @@ void writesolution (void)
         line[1024];     /* static line to build solution vector */
     size_t clen = 0, wlen = 0, ccvted = 0, wcvted = 0, i;
     bool havevalue = FALSE;
-	
+
     /* get text from edit box */
     SendMessage(hWndEdit, WM_GETTEXT, iTextSize, (LPARAM)szText);
-    
+
     wlen = wcslen (szText);     /* get wide character length */
     clen = wlen * 2;            /* double size for C-string */
     cstr = new char[clen];      /* allocate storage for C-string */
-    
+
     /* convert wide-char to C-string */
     wcstombs_s (&ccvted, cstr, wlen, szText, _TRUNCATE);
     p = cstr;
-    
+
     /* advance start of first value in system of equations */
     do {
         if ( *p == '.' || *p == '-' || *p == '+' || isdigit (*p)) {
@@ -372,58 +376,58 @@ void writesolution (void)
             break;
         }
     } while (*p++);
-    
+
     if (havevalue) {
         /* fill matrix from values in buffer */
         mtrx_t *m = mtrx_read_alloc_buf (p);
-        
+
         /* solve system by gauss-jordan elimintation will full-pivoting */
         mtrx_solv_gaussj (m->mtrx, m->rows);    /* m->mtrx now contains
                                                 * inverse + solution vector
                                                 */
-        
+
         /* realloc C-string to append Solution Vector */
-        cstr = (char *)szrealloc (cstr, sizeof *cstr, clen, 
+        cstr = (char *)szrealloc (cstr, sizeof *cstr, clen,
                                     clen + (m->rows + 1) * RESULTSZ);
-        
+
         /* append solution vector heading */
         strcat (cstr, "\r\n\r\nSolution Vector:\r\n\r\n");
-        
+
         for (i = 0; i < m->rows; i++) { /* appending individual components */
-            sprintf (line, " x[%3" SZTFMT "] : % 11.7f\r\n", 
+            sprintf (line, " x[%3" SZTFMT "] : % 11.7f\r\n",
                         i, m->mtrx[i][m->rows]);
             strcat (cstr, line);
         }
-        
+
         clen = strlen (cstr);           /* get new length of C-string */
         szSolv = new wchar_t[clen + 1]; /* allocate for conversion to wchar_t */
-        
+
         /* convert C-string to wchar_t */
         mbstowcs_s (&wcvted, szSolv, clen + 1, cstr, _TRUNCATE);
-        
+
         /* write to editbox */
         SendMessage (hWndEdit, WM_SETTEXT, 0, (LPARAM)szSolv);
     }
     else {
         clen = strlen (cstr);   /* get new length of C-string */
-        
+
         /* realloc C-string to append error message */
-        cstr = (char *)szrealloc (cstr, sizeof *cstr, clen, 
+        cstr = (char *)szrealloc (cstr, sizeof *cstr, clen,
                                     clen + 2 * RESULTSZ);
-        
+
         /* append error text */
         strcat (cstr, "\r\n\r\n ERROR Invalid Matrix Format\r\n");
-        
+
         clen = strlen (cstr);           /* get new length of C-string */
         szSolv = new wchar_t[clen + 1]; /* allocate for conversion to wchar_t */
-        
+
         /* convert C-string to wchar_t */
         mbstowcs_s (&wcvted, szSolv, clen + 1, cstr, _TRUNCATE);
-        
+
         /* write to editbox */
         SendMessage (hWndEdit, WM_SETTEXT, 0, (LPARAM)szSolv);
     }
-    
+
     delete [] szText;   /* free allocated memory */
     delete [] szSolv;
     delete [] cstr;
